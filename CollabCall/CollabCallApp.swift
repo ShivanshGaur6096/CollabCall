31285b8e5c3ff5db455b7a1028c51643811b968b
Shivansh Gaur <shivansh.gaur@blackngreen.com>
2025-03-24 22:21:55 +0530

    Created an application

---
+++ CollabCall/CollabCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabCallApp.swift
+//  CollabCall
+//
+//  Created by Shivansh Gaur on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CollabCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
